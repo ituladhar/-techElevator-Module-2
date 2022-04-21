7520cccdda15e1f3d8a7462f997d0f24e103faad
Ishwor Tuladhar <ishwor.tuladhar@gmail.com>
2022-04-20 21:31:10 -0400

    01_Intro_to _Database_Submission

--- 01_Intro_to_Databases/exercise/Exercises/02_cities_Pennsylvania.sql
+++ 01_Intro_to_Databases/exercise/Exercises/02_cities_Pennsylvania.sql
@@ -1,2 +1,4 @@
 -- 2. The name and area of all cities in Pennsylvania (PA) (4 rows)
-
+SELECT city_name, area
+FROM city
+where state_abbreviation = 'PA';
--- 01_Intro_to_Databases/exercise/Exercises/08_small_CA_cities.sql
+++ 01_Intro_to_Databases/exercise/Exercises/08_small_CA_cities.sql
@@ -1,2 +1,5 @@
 -- 8. The name and population of cities in California (CA) with a population less than 150,000 people (37 rows)
-
+SELECT city_name, population
+FROM city
+Where state_abbreviation = 'CA'
+And population < 150000;
